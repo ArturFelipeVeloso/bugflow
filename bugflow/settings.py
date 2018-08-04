778516186a217b0f77bf43f4f82e279398210ded
Artur Veloso <arturfdasveloso@gmail.com>
2018-08-04 16:15:30 -0300

    Correção do bootstrap

--- bugflow/settings.py
+++ bugflow/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'bugflow', 'starticfiles'),
+    os.path.join(BASE_DIR, 'bugflow', 'static'),
 ]
 
 STATIC_URL = '/static/'
